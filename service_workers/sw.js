b8ba89702dd84093c5499cacabef98f72db57705
mangogan <1261639871@qq.com>
2018-06-24 23:07:33 +0800

    feature: 添加http劫持与失败重发

--- service_workers/sw.js
+++ service_workers/sw.js
@@ -1,4 +1,6 @@
+//新增了一个install事件监听器
 this.addEventListener('install', function(event) {
+  //使用ExtendableEvent.waitUntil方法确保Service Worker不会在wU里面的代码执行完之前，安装完成
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
@@ -10,3 +12,12 @@ this.addEventListener('install', function(event) {
     })
   );
 });
+
+//使用fetch事件监听器来劫持http响应
+this.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).catch(function() {
+      return fetch(event.request);
+    })
+  );
+})
