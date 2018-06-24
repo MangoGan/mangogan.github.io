188df375eb9c894949d039c1aadcf4c4239ad522
mangogan <1261639871@qq.com>
2018-06-24 22:27:20 +0800

    test: service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/service_workers/',
+        '/service_workers/index.html',
+        '/service_workers/app.js',
+        '/service_workers/img/lalala.jpg'
+      ]);
+    })
+  );
+});
